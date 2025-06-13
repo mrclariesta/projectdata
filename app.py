dcd19ea8fd167686ae90f67fc9ee091f7ddcaa50
Marcel Ariesta <167510397+mrclariesta@users.noreply.github.com>
2025-06-14 05:00:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import xgboost
+import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from sklearn.linear_model import Lasso, LinearRegression
